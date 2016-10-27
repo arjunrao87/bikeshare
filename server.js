c6bf1551675dacafab724ecfe66d965eeb2cfffb
arjrao <arjun.rao1@blackrock.com>
2016-10-27 14:51:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');  
 var request = require('request');
-
 var app = express();
-console.log( "Starting up...");
+
+app.listen(process.env.API_PORT || 3100);  
+
+console.log( "Started up Express Server");
 
 app.use('/data', function(req, res) {  
-	console.log( "Requesting data from bay area bike share..." );
+	console.log( "Requesting data from Bay Area Bike Share..." );
 	var url = "http://feeds.bayareabikeshare.com/stations/stations.json";
 	req.pipe(request(url)).pipe(res);
 });
-
-app.listen(process.env.API_PORT || 3100);  
