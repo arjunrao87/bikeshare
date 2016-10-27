edfcb93032fd38dd62e46fd57159242603dc398d
arjrao <arjun.rao1@blackrock.com>
2016-10-27 16:08:38 -0400

    Using some state vars

--- client/src/components/BikeShare.js
+++ client/src/components/BikeShare.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import ServerCallAPI from './ServerCallAPI';
+import ServerCallAPI from '../helpers/ServerCallAPI';
 
 class BikeShare extends Component {
 
 	constructor( props ){
 		super(props);
 		this.queryAPI();
+		this.state = {data:[]};
 	}
 
 	queryAPI(){
 		console.log( "QueryAPI called..." );
 		ServerCallAPI.retrieveBikeData((data) => {
-			console.log( data );
+			this.setState({data:data});
+			return data;
 		});
 	}
 
 	render() {
 	  return (
 	    <div>
-	    	<pre>Hello World</pre>
+	    	<pre>{this.state.data.executionTime}</pre>
 	    </div>
 	  );
 	}
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BikeShare from './BikeShare';
+import BikeShare from './components/BikeShare';
 
 ReactDOM.render( <BikeShare/>, document.getElementById('root') );
