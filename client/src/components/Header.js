13afb68ef43b49615d8f1c5aba4bae8868fa372c
arjrao <arjun.rao1@blackrock.com>
2016-11-18 18:01:18 -0500

    Adding modal in header bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,45 @@
 import React from 'react';
+import {ModalContainer, ModalDialog} from 'react-modal-dialog';
 
 class Header extends React.Component{
 
+	constructor( props ){
+			super(props);
+		  this.state={isShowingModal: false};
+			this.handleClick = this.handleClick.bind( this );
+			this.handleClose = this.handleClose.bind( this );
+	}
+
+	handleClick() {
+		console.log( "this.cllick ");
+		this.setState({isShowingModal: true})
+	};
+
+	handleClose() {this.setState({isShowingModal: false})};
+
 	render(){
 		var executionTime = this.props.time;
+
 		return(
 			<div>
 			<nav className="navbar navbar-default navbar-fixed-top">
 				<div className="container-fluid">
-					<div className="navbar-header">
-						 <a href="/about" className="navbar-brand">Bay Area Bike Share</a>
+					<div className="navbar-header upfront" onClick={this.handleClick}>
+						 <a className="navbar-brand">
+						 	Bay Area Bike Share
+					      {
+					        this.state.isShowingModal &&
+					        <ModalContainer onClose={this.handleClose}>
+					          <ModalDialog onClose={this.handleClose}>
+											<h2>About Bay Area Bike Share</h2>
+											<p>Provide availability of bikes along the Bay Area biking route.</p>
+											<br/>
+											<h2>About the Creator</h2>
+											<p>Website made by Arjun Rao. Visit www.arjunrao.co for more information</p>
+					          </ModalDialog>
+					        </ModalContainer>
+					      }
+						 </a>
 					</div>
 				  <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-6">
 					 <ul className="nav navbar-nav navbar-right">
